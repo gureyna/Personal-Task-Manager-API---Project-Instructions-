b8215bdf1cf596e50b0f35dbd731824cbda961a4
gureyna <gureynaboy@gmail.com>
2024-11-05 02:37:21 +0300

    Initial commit

---
+++ controllers/taskController.js
@@ -0,0 +1,44 @@
+exports.createTask = (req, res) => {
+    const form = new IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({ message: 'Error parsing form' }));
+            return;
+        }
+        const image = files.image[0]
+        
+        const tasks = readTasksFromFile();
+        const newTask = {
+            id: Date.now(),
+            title: fields.title,
+            description: fields?.description || '',
+            status: fields?.status || 'pending',
+            Image: image ? `/uploads/${image.NewFilename}` : null,
+        };
+
+        tasks.push(newTask);
+        writeTasksToFile(tasks);
+
+        if(files.image) {
+            copyFileSync(files.image.path, path.join(__dirname, '../uploads', files.image.name));
+            res.end(JSON.stringify(newTask))
+            
+        }
+    })
+};
+exports.createTask = (req, res) => {
+
+}
+exports.updateTask = (req, res) => {
+    res.end(JSON.stringify({
+        message: 'Update Task'
+    }))
+}
+exports.deleteTask = (req, res) => {
+    res.end(JSON.stringify({
+        message: 'Delete Task'
+    }))
+}
+        
---
+++ utils/fileHandler.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const filepath = './data/tasks.json';
+
+exports.writeTasksToFile = (tasks) => {
+    fs.writeFileSync(filepath, JSON.stringify(tasks));
+}
+
+exports.readTasksFromFile = () => {
+    if (!fs.existsSync(filepath)) {
+        this.writeTasksToFile([]);
+    }
+    const data = fs.readFileSync(filepath);
+    return JSON.parse(data);
+    
+}
